dec58570587a57f11c515b4fea638f7d6ac0de66
Ayo <edwinkayodeayo@gmail.com>
2023-07-13 22:17:18 -0700

    c learning

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #inlude < stdio.h >
-/**/
+/*
+ *main - putn out "Programming is like building a multilingual puzzle
+ */
 int main(void)
 {
 	putchar("Programming is like building a multilingual puzzle\n");
