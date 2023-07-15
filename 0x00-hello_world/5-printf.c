69ac2ff2554f38d2ef0901f992b3a3259768e4b6
Ayo <edwinkayodeayo@gmail.com>
2023-07-15 04:34:22 -0700

    c learning

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
-*main - entry point
-*return (succeed)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (void);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
 }
