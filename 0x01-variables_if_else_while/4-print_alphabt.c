4a63eceef29d7e1bb9edfc9594150362aab5894f
Ayo <edwinkayodeayo@gmail.com>
2023-07-21 12:25:42 -0700

    if command

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,12 @@ int main(void)
 	char c;
 
 	c = 'a';
-	while
-		(c <= 'z')
+	while(c <= 'z')
 		{
 			if (c != 'q' && c != 'e')
 				putchar(c);
-				c++;
 		}
+	c++;
 	putchar('\n');
 		return (0);
 }
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always to 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = '0';
+		if (i <= 9, i++)
+		{
+			printf("%d", i);
+		}
+	printf("\n");
+	return (0);
+}
